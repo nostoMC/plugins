919d7a158e67f9cfa71f4ffea363292ee41f457d
itsRed-v2 <paul.charignon.v2@gmail.com>
2021-07-27 20:03:37 +0200

    Revert "Gros refactoring du plugin principal: rangement des class et packages + ajout de"
    This reverts commit 9e0e0f5ce78c88a05ef64fa2ea76db5cc278557e.

---
+++ Nosto/commands/fr/djredstone/nosto/feed/CommandFeed.java
@@ -0,0 +1,37 @@
+package fr.djredstone.nosto.feed;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandFeed implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		Player player = (Player) sender;
+
+		if (args.length == 0) {
+            player.setFoodLevel(20);
+            player.sendMessage("");
+            player.sendMessage("§2Votre barre de nourriture a été restaurer !");
+            return true;
+        }
+        Player target = Bukkit.getServer().getPlayer(args[0]);
+        if (target == null) {
+        	player.sendMessage("");
+            player.sendMessage("§cJoueur innconu");
+            return true;
+        }
+        target.setFoodLevel(20);
+        target.sendMessage("");
+        target.sendMessage("§2Votre barre de nourriture a été restaurer !");
+        player.sendMessage("");
+        player.sendMessage("§2La barre de nourriture de §a§l" + target.getName() + " §2a été restaurer !");
+		
+		return false;
+	}
+
+}
