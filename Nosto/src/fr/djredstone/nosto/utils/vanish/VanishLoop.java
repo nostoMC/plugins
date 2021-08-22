cc5d653adb7e7dffc0a7ed9da18bc3f7c9a35731
Dj_Redstone_64 <djredstone64@gmail.com>
2021-08-22 12:01:09 +0200

    Fix rapide d'une variable public

--- Nosto/src/fr/djredstone/nosto/utils/vanish/VanishLoop.java
+++ Nosto/src/fr/djredstone/nosto/utils/vanish/VanishLoop.java
@@ -1,7 +1,5 @@
 package fr.djredstone.nosto.utils.vanish;
 
-import java.util.ArrayList;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -9,8 +7,6 @@ import org.bukkit.scheduler.BukkitRunnable;
 import fr.djredstone.nosto.Main;
 
 public class VanishLoop {
-	
-	ArrayList<Player> vanishList = Main.getVanishList();
 
 	public VanishLoop(Main main) {
 		
@@ -20,8 +16,7 @@ public class VanishLoop {
 			public void run() {
 				
 				for(Player players : Bukkit.getOnlinePlayers()) {
-					vanishList = Main.getVanishList();
-					if(vanishList.contains(players)) {
+					if(Main.vanishList.contains(players)) {
 						players.setInvisible(true);
 					} else {
 						players.setInvisible(false);
