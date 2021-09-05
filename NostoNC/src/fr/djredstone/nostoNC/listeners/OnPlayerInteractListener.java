edcb95ef81cc9e2cf82a4352651c925151103af3
DjRedstone <djredstone64@gmail.com>
2021-09-05 14:59:07 +0200

    NC - Tous les joueurs peuvent ouvrir les portes et interagir avec les panneaux

--- NostoNC/src/fr/djredstone/nostoNC/listeners/OnPlayerInteractListener.java
+++ NostoNC/src/fr/djredstone/nostoNC/listeners/OnPlayerInteractListener.java
@@ -1,9 +1,11 @@
 package fr.djredstone.nostoNC.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -14,6 +16,8 @@ public class OnPlayerInteractListener implements Listener {
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		if(event.getPlayer().getWorld() == Bukkit.getWorld("Nightclub")) {
 			if(!event.getPlayer().hasPermission("nosto.nightclub.interact")) event.setCancelled(true);
+			if(event.getClickedBlock().getType() == Material.OAK_WALL_SIGN) event.setCancelled(false);
+			if(event.getClickedBlock().getType() == Material.DARK_OAK_DOOR) event.setCancelled(false);
 		}
 	}
 	
@@ -31,5 +35,12 @@ public class OnPlayerInteractListener implements Listener {
 			if(event.getEntity() instanceof ArmorStand) event.setCancelled(true);
 		}
 	}
+	
+	@EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+		if(event.getPlayer().getWorld() == Bukkit.getWorld("Nightclub")) {
+			if(!event.getPlayer().hasPermission("nosto.nightclub.interact")) event.setCancelled(true);
+		}
+    }
 
 }
