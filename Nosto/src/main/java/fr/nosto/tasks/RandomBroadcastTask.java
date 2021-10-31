d222c3d84c1e1fb50faf09dbff7dbb6f78ef19fb
itsRed-v2 <paul.charignon.v2@gmail.com>
2021-10-31 15:55:47 +0100

    Main - déplacage d'une déclaration de variable dans la class RandomBroadcastTask

--- Nosto/src/main/java/fr/nosto/tasks/RandomBroadcastTask.java
+++ Nosto/src/main/java/fr/nosto/tasks/RandomBroadcastTask.java
@@ -23,16 +23,17 @@ public class RandomBroadcastTask {
 		inited = true;
 		
 		String[] messages = {"1", "2", "3", "4"};
+
+		Set<String> survies_names = new HashSet<>();
+		survies_names.add("survie");
+		survies_names.add("survie_the_end");
+		survies_names.add("survie_nether");
     	
 		new BukkitRunnable() {
 		    @Override
 		    public void run() {
 				if(!Bukkit.getOnlinePlayers().isEmpty()) {
 					for(Player players : Bukkit.getOnlinePlayers()) {
-						Set<String> survies_names = new HashSet<>();
-		    			survies_names.add("survie");
-		    			survies_names.add("survie_the_end");
-		    			survies_names.add("survie_nether");
 		    			if(survies_names.contains(players.getWorld().getName())) {
 							if(messages[new Random().nextInt(messages.length)].equals("1")) {
 				    			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + players.getName() + " [\"\",{\"text\":\"\\n\"},{\"text\":\"Si vous rencontrez des bugs, veuillez nous les signaler sur le discord :\",\"bold\":true,\"color\":\"yellow\"},{\"text\":\" \",\"bold\":true,\"color\":\"gold\"},{\"text\":\"https://discord.io/Nosto\",\"bold\":true,\"underlined\":true,\"color\":\"gold\",\"clickEvent\":{\"action\":\"open_url\",\"value\":\"https://discord.io/Nosto\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Cliquez pour accéder au serveur discord\"}}]");
