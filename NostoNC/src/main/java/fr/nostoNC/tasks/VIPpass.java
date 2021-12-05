77236e06a75e12d75a91ee135e65409a02ed3f75
itsRed-v2 <paul.charignon.v2@gmail.com>
2021-12-05 18:09:43 +0100

    NC - déplacement du mécanisme du golem dans la nouvelle map

--- NostoNC/src/main/java/fr/nostoNC/tasks/VIPpass.java
+++ NostoNC/src/main/java/fr/nostoNC/tasks/VIPpass.java
@@ -1,43 +1,46 @@
 package fr.nostoNC.tasks;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.BoundingBox;
 import org.bukkit.util.Vector;
 
 import fr.nostoNC.Main;
 
 public class VIPpass {
 
+	private static final BoundingBox box = BoundingBox.of(new Vector(-1f, 101f, 182f), new Vector(0f, 104f, 185f));
+	private static int soundCooldown = 0;
+
 	public VIPpass(Main main) {
 
 		new BukkitRunnable() {
-			
+
 			@Override
 			public void run() {
 
-				for(Player player : Bukkit.getOnlinePlayers()) {
-					
-					if(player.getWorld() == Main.defaultWorld && !player.hasPermission("nosto.nightclub.vip")) {
-						
+				for(Player player : Main.defaultWorld.getPlayers()) {
+
+					if(!player.hasPermission("nosto.nightclub.vip")) {
+
 						Location loc = player.getLocation();
-						if(loc.getBlockX() > -8 && loc.getBlockX() < -5) {
-							if(loc.getBlockY() > 63 && loc.getBlockY() < 68) {
-								if(loc.getBlockZ() > -37 && loc.getBlockZ() < -32) {
-									Main.defaultWorld.playSound(loc, Sound.ENTITY_IRON_GOLEM_HURT, 100, 1);
-									player.setVelocity(new Vector(1, 0.5, -0.4));
-								}
+						if (box.contains(loc.toVector())) {
+							if (soundCooldown == 0) {
+								Main.defaultWorld.playSound(loc, Sound.ENTITY_IRON_GOLEM_HURT, 1, 1);
+								soundCooldown = 2;
 							}
+							player.setVelocity(new Vector(-.8, .5, .2));
 						}
-						
+
 					}
 					
 				}
+				if (soundCooldown > 0) soundCooldown--;
 				
 			}
-		}.runTaskTimer(main, 0, 1);
+		}.runTaskTimer(main, 5, 1);
 
 	}
 
