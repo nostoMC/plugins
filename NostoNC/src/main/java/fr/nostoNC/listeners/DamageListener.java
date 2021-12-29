a5ebcbf4ca88ceb85a9348804fdfef986cf1b802
itsRed-v2 <paul.charignon.v2@gmail.com>
2021-12-29 11:16:08 +0100

    NC - cancel des dégats de suffocation et de la mort

--- NostoNC/src/main/java/fr/nostoNC/listeners/DamageListener.java
+++ NostoNC/src/main/java/fr/nostoNC/listeners/DamageListener.java
@@ -3,13 +3,22 @@ package fr.nostoNC.listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 import fr.nostoNC.Main;
 
 public class DamageListener implements Listener {
 
     @EventHandler
-    public void onDamageByBlock(EntityDamageEvent event) {
+    public void onDamageBySuffocation(EntityDamageEvent event) {
+        if (event.getEntity().getWorld().getName().equals(Main.defaultWorld.getName())
+                && event.getCause() == EntityDamageEvent.DamageCause.SUFFOCATION) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
         if (event.getEntity().getWorld().getName().equals(Main.defaultWorld.getName())) {
             event.setCancelled(true);
         }
