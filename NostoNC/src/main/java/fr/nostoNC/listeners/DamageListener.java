e603e317947f677d3568a5bf0272ed477c4759ce
itsRed-v2 <paul.charignon.v2@gmail.com>
2021-12-31 17:59:07 +0100

    NC - cancel des dégats de pvp et des explosions de firework

--- NostoNC/src/main/java/fr/nostoNC/listeners/DamageListener.java
+++ NostoNC/src/main/java/fr/nostoNC/listeners/DamageListener.java
@@ -1,7 +1,9 @@
 package fr.nostoNC.listeners;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
@@ -13,6 +15,17 @@ public class DamageListener implements Listener {
     public void onDamageBySuffocation(EntityDamageEvent event) {
         if (Utils.isInClub(event.getEntity()) && event.getCause() == EntityDamageEvent.DamageCause.SUFFOCATION) {
             event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onDamageByEntity(EntityDamageByEntityEvent event) {
+        if (Utils.isInClub(event.getEntity())) {
+
+            EntityType damagerType = event.getDamager().getType();
+            if (damagerType == EntityType.FIREWORK || damagerType == EntityType.PLAYER) {
+                event.setCancelled(true);
+            }
         }
     }
 
