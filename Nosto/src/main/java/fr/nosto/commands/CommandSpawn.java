d056b9e4e028f341c8756eec583779b7f36a42d1
DjRedstone <baptistemay3012@gmail.com>
2022-02-07 14:01:46 +0100

    Main - clean de la commande /spawn

--- Nosto/src/main/java/fr/nosto/commands/CommandSpawn.java
+++ Nosto/src/main/java/fr/nosto/commands/CommandSpawn.java
@@ -1,13 +1,17 @@
 package fr.nosto.commands;
 
-import org.bukkit.Bukkit;
+import fr.nosto.Main;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
 
 public class CommandSpawn implements CommandExecutor {
 
@@ -15,53 +19,35 @@ public class CommandSpawn implements CommandExecutor {
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
 		
 		Player player = (Player) sender;
-		
-		Location SurvivalLobby = new Location(Bukkit.getWorld("survie"), -0.5, 63, 0.5, 180f, 0f);
-		Location skyblockLobby = new Location(Bukkit.getWorld("skyworld"), 0.5, 150, 4.5, 0f, -10f);
-		Location freebuildLobby = new Location(Bukkit.getWorld("Freebuild"), 107.5, 63, 0.5, 0f, 0f);
-		Location huntLobby = new Location(Bukkit.getWorld("Hunt"), -127, 64, 160, 0f, 0f);
-		Location pvpLobby = new Location(Bukkit.getWorld("event"), 136, 70, -231, 0f, 0f);
-		Location lgLobby = new Location(Bukkit.getWorld("lg"), 2841, 72, 3536, 0f, 0f);
-		Location roueLobby = new Location(Bukkit.getWorld("RoueDeLaChance"), 0, 229, 50, 0f, 0f);
-		Location showLobby = new Location(Bukkit.getWorld("show"), 0.5, 65, 0.5, 0f, 0f);
-		
-		if (player.getWorld() == Bukkit.getWorld("survie")) {
-			player.sendMessage("");
-			player.sendMessage("§eTéléportation au spawn !");
-			player.teleport(SurvivalLobby);
-		} else if (player.getWorld() == Bukkit.getWorld("skyworld")) {
-			player.sendMessage("");
-			player.sendMessage("§eTéléportation au spawn !");
-			player.teleport(skyblockLobby);
-		} else if (player.getWorld() == Bukkit.getWorld("Freebuild")) {
-			player.sendMessage("");
-			player.sendMessage("§eTéléportation au spawn !");
-			player.teleport(freebuildLobby);
-		} else if (player.getWorld() == Bukkit.getWorld("Hunt")) {
-			player.sendMessage("");
-			player.sendMessage("§eTéléportation au spawn !");
-			player.teleport(huntLobby);
-		} else if (player.getWorld() == Bukkit.getWorld("event")) {
-			player.sendMessage("");
-			player.sendMessage("§eTéléportation au spawn !");
-			player.teleport(pvpLobby);
-		} else if (player.getWorld() == Bukkit.getWorld("lg")) {
-			player.sendMessage("");
-			player.sendMessage("§eTéléportation au spawn !");
-			player.teleport(lgLobby);
-		} else if (player.getWorld() == Bukkit.getWorld("RoueDeLaChance")) {
-			player.sendMessage("");
-			player.sendMessage("§eTéléportation au spawn !");
-			player.teleport(roueLobby);
-		} else if (player.getWorld() == Bukkit.getWorld("show")) {
-			player.sendMessage("");
-			player.sendMessage("§eTéléportation au spawn !");
-			player.teleport(showLobby);
-		} else {
-			player.sendMessage("");
-			player.sendMessage("§cAucun spawn n'existe pour ce monde !");
+
+		File file = new File(Main.instance.getDataFolder()+"/spawn.yml");
+		FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
+
+		fileConfiguration.set("coo.EXEMPLE.x", 0.0);
+		fileConfiguration.set("coo.EXEMPLE.y", 0.0);
+		fileConfiguration.set("coo.EXEMPLE.z", 0.0);
+		fileConfiguration.set("coo.EXEMPLE.yaw", 0.0);
+		fileConfiguration.set("coo.EXEMPLE.pitch", 0.0);
+		try {
+			fileConfiguration.save(file);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
+
+		String worldName = player.getWorld().getName();
+
+		if (fileConfiguration.contains("coo." + worldName)) {
+			double x = fileConfiguration.getDouble("coo." + worldName + ".x");
+			double y = fileConfiguration.getDouble("coo." + worldName + ".y");
+			double z = fileConfiguration.getDouble("coo." + worldName + ".z");
+			float yaw = (float) fileConfiguration.getDouble("coo." + worldName + ".yaw");
+			float pitch = (float) fileConfiguration.getDouble("coo." + worldName + ".pitch");
+			player.sendMessage("\n§7§oTéléportation au spawn du monde...");
+			player.teleport(new Location(player.getWorld(), x, y, z, yaw, pitch));
+		} else {
+			player.sendMessage("\n§cAucun spawn n'existe pour ce monde !");
+		}
+
 		return false;
 	}
 
