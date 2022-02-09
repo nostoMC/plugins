cd3fd45b9d92de3a7c6eda9eb4aaaa6d56e2a1cd
itsRed-v2 <paul.charignon.v2@gmail.com>
2022-02-09 10:07:38 +0100

    Main - réorganisation de RandomBroadcastTask

--- Nosto/src/main/java/fr/nosto/tasks/RandomBroadcastTask.java
+++ Nosto/src/main/java/fr/nosto/tasks/RandomBroadcastTask.java
@@ -3,48 +3,42 @@ package fr.nosto.tasks;
 import java.util.ArrayList;
 import java.util.Random;
 
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import fr.nosto.Main;
 import fr.nosto.Utils;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class RandomBroadcastTask {
 
 	static Random random = new Random();
 
+	private static final ArrayList<TextComponent> messagesList = new ArrayList<>();
+	static {
+		TextComponent message1 = new TextComponent("\n");
+		message1.addExtra("§e§l" + "Si vous rencontrez des bugs, veuillez nous les signaler sur le discord : ");
+		TextComponent link = new TextComponent("§6§l§n" + "https://discord.io/Nosto");
+		link.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, "https://discord.io/Nosto"));
+		message1.addExtra(link);
+
+		TextComponent message2 = new TextComponent("\n");
+		message2.addExtra("§c§l" + "Les machines à lag sont ");
+		message2.addExtra("§4§l" + "strictement interdites ");
+		message2.addExtra("§c§l" + "en tous mondes.");
+
+		messagesList.add(message1);
+		messagesList.add(message2);
+	}
+
 	public static void init(Main main) {
-
-		ArrayList<TextComponent> messagesList = new ArrayList<>();
-
-		messagesList.add(message1());
-		messagesList.add(message2());
-
 		new BukkitRunnable() {
 			@Override
 			public void run() {
 				TextComponent textComponent = messagesList.get(random.nextInt(messagesList.size()));
 				Utils.sendTextComponentToSurvival(textComponent);
 			}
-		}.runTaskTimer(main, 0, 3000);
-	}
-
-	private static TextComponent message1() {
-		TextComponent finalTextComponent = new TextComponent("\n");
-		finalTextComponent.addExtra("§e§l" + "Si vous rencontrez des bugs, veuillez nous les signaler sur le discord : ");
-		TextComponent link = new TextComponent("§6§l§n" + "https://discord.io/Nosto");
-		link.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, "https://discord.io/Nosto"));
-		finalTextComponent.addExtra(link);
-		return finalTextComponent;
-	}
-
-	private static TextComponent message2() {
-		TextComponent finalTextComponent = new TextComponent("\n");
-		finalTextComponent.addExtra("§c§l" + "Les machines à lag sont ");
-		finalTextComponent.addExtra("§4§l" + "strictement interdites ");
-		finalTextComponent.addExtra("§c§l" + "en tous mondes.");
-		return finalTextComponent;
+		}.runTaskTimer(main, 0, 3600);
 	}
 
 }
