4d8f4ad011cfc26693b14e93f0c0d49171fa94a0
itsRed-v2 <paul.charignon.v2@gmail.com>
2022-02-10 18:44:03 +0100

    Main - amélioration du tabcompleter de la commande Event

--- Nosto/src/main/java/fr/nosto/commands/TabEvent.java
+++ Nosto/src/main/java/fr/nosto/commands/TabEvent.java
@@ -11,29 +11,28 @@ import org.jetbrains.annotations.NotNull;
 
 public class TabEvent implements TabCompleter {
 	
-	List<String> arguments = new ArrayList<>();
-	List<String> arguments2 = new ArrayList<>();
+	private static final List<String> arguments = new ArrayList<>();
+	static {
+		arguments.add("add");
+		arguments.add("stop");
+	}
+	private static final List<String> arguments2 = new ArrayList<>();
+	static {
+		arguments2.add("nostoclub");
+		arguments2.add("mainlobby");
+	}
 
 	@Override
 	public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
-
-		if (arguments.isEmpty()) {
-			arguments.add("add");
-			arguments.add("stop");
-		}
-		if (arguments2.isEmpty()) {
-			arguments2.add("nostoclub");
-			arguments2.add("mainlobby");
-		}
 		
 		List<String> result = new ArrayList<>();
+
 		if (args.length == 1) {
 			for (String a : arguments) {
 				if (a.toLowerCase().startsWith(args[0].toLowerCase())) {
 					result.add(a);
 				}
 			}
-			return result;
 		} else if (args.length == 2) {
 			if (args[0].equalsIgnoreCase("add")) {
 				for (String a : arguments2) {
@@ -41,14 +40,10 @@ public class TabEvent implements TabCompleter {
 						result.add(a);
 					}
 				}
-				return result;
 			}
-		} else {
-			result.add("");
-			return result;
 		}
 		
-		return null;
+		return result;
 	}
 
 }
