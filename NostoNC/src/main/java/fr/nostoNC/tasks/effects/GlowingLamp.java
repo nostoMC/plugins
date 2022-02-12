42639551a7cb7e786c3d1248f89017aa4f308bf9
itsRed-v2 <paul.charignon.v2@gmail.com>
2022-02-12 17:06:05 +0100

    NC - réécriture de l'effet GlowingLamp

--- NostoNC/src/main/java/fr/nostoNC/tasks/effects/GlowingLamp.java
+++ NostoNC/src/main/java/fr/nostoNC/tasks/effects/GlowingLamp.java
@@ -1,46 +1,26 @@
 package fr.nostoNC.tasks.effects;
 
-import fr.nostoNC.Main;
-import fr.nostoNC.Utils;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import fr.nostoNC.Main;
+import fr.nostoNC.Utils;
 
 public class GlowingLamp {
 
-    private static final List<Location> all = new ArrayList<>();
+    enum Axis {
+        X, Z;
 
-    private static final HashMap<Integer, ArrayList<Location>> lignes = new HashMap<>();
-    private static final HashMap<Integer, ArrayList<Location>> colone = new HashMap<>();
+        public int getMaxIndex() {
+            return this == X ? Xwide : Zwide;
+        }
+    }
+    enum Direction { FRONT, BACK }
+
+    private static final int Zwide = 13, Xwide = 15;
 
     public static void setup() {
-
-        int lMax = 13;
-        int LMax = 15;
-
-        for (int l = 0; l < lMax*2; l += 2) {
-            for (int L = 0; L < LMax*2; L += 2) {
-                Location location = new Location(Utils.getDefaultWorld(), -16 + L, 113, 150 + l);
-                all.add(location);
-                if (!lignes.containsKey(l/2)) lignes.put(l/2, new ArrayList<>());
-                ArrayList<Location> list = lignes.get(l/2);
-                list.add(location);
-                lignes.put(l/2, list);
-            }
-        }
-
-        for (int i = 0; i < LMax; i++) {
-            ArrayList<Location> list = new ArrayList<>();
-            for (int i1 = 0; i1 < lignes.size(); i1++) {
-                list.add(lignes.get(i1).get(i));
-            }
-            colone.put(i, list);
-        }
 
         allChangeDimmer(true);
         allChangeDimmer(false);
@@ -51,50 +31,80 @@ public class GlowingLamp {
             @Override
             public void run() {
                 switch (stade) {
-                    case 0, 2 -> allWave(lignes, false, 3);
-                    case 1, 3 -> allWave(lignes, true, 3);
-                    case 4, 6 -> allWave(colone, false, 2);
-                    case 5, 7 -> allWave(colone, true, 2);
+                    case 0, 2 -> allWave(Axis.Z, Direction.BACK, 3);
+                    case 1, 3 -> allWave(Axis.Z, Direction.FRONT, 3);
+                    case 4, 6 -> allWave(Axis.X, Direction.BACK, 2);
+                    case 5, 7 -> allWave(Axis.X, Direction.FRONT, 2);
                 }
                 stade ++;
                 if (stade == 8) stade = defaultInt;
             }
-        }.runTaskTimer(Main.getInstance(), 0, 5*8);
+        }.runTaskTimer(Main.getInstance(), 5, 5*8);
 
     }
 
-    private static void allChangeDimmer(boolean bool) {
-        for (Location location : all) {
-            Block b = Utils.getDefaultWorld().getBlockAt(location);
-            if (bool) b.setType(Material.GLOWSTONE);
-            else b.setType(Material.STONE);
+    private static void setLamp(int x, int z, boolean on) {
+        if (x >= Xwide || z >= Zwide || x < 0 || z < 0) return;
+
+        Location loc = new Location(Utils.getDefaultWorld(), -16, 113, 150);
+        loc.add(x*2, 0, z*2);
+
+        if (on) loc.getBlock().setType(Material.GLOWSTONE);
+        else loc.getBlock().setType(Material.STONE);
+    }
+
+    private static void setLine(Axis axis, int index, boolean on) {
+        int maxValue = axis.getMaxIndex();
+
+        for (int i = 0; i < maxValue; i++) {
+            switch (axis) {
+                case X -> setLamp(i, index, on);
+                case Z -> setLamp(index, i, on);
+            }
         }
     }
 
-    private static void allWave(HashMap<Integer, ArrayList<Location>> hashMap, boolean direction, int timing) {
-        int i;
-        if (direction) i = -1;
-        else i = hashMap.size();
+    private static void allChangeDimmer(boolean bool) {
+        for (int x = 0; x < Xwide; x++) {
+            for (int z = 0; z < Zwide; z++) {
+                setLamp(x, z, bool);
+            }
+        }
+    }
+
+    private static void allWave(Axis axis, Direction dir, int timing) {
+        int maxProgress = axis.getMaxIndex();
+        Axis lineAxis = switch (axis) {
+            case X -> Axis.Z;
+            case Z -> Axis.X;
+        };
+
         new BukkitRunnable() {
-            int t = i;
+            int progress = switch (dir) {
+                case FRONT -> 0;
+                case BACK -> maxProgress - 1;
+            };
+
             @Override
             public void run() {
-                if (direction) {
-                    if (t > hashMap.size()) return;
-                    t++;
-                } else {
-                    if (t < 0) return;
-                    t--;
+                if (progress < 0 || progress >= maxProgress) {
+                    this.cancel();
+                    return;
                 }
-                if (hashMap.containsKey(t)) for (Location location : hashMap.get(t)) {
-                    Block block = Utils.getDefaultWorld().getBlockAt(location);
-                    block.setType(Material.GLOWSTONE);
-                    new BukkitRunnable() {
-                        @Override
-                        public void run() {
-                            block.setType(Material.STONE);
-                        }
-                    }.runTaskLater(Main.getInstance(), timing* 3L);
+
+                setLine(lineAxis, progress, true);
+
+                int progressClone = progress;
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        setLine(lineAxis, progressClone, false);
+                    }
+                }.runTaskLater(Main.getInstance(), timing * 3L);
+
+                switch (dir) {
+                    case FRONT -> progress++;
+                    case BACK -> progress--;
                 }
             }
         }.runTaskTimer(Main.getInstance(), 0, timing);
